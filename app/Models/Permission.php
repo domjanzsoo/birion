a0b0d05191093cad0edefd77a909119d7379e93e
Zsolt Domjan <domjanzsoo@yahoo.com>
2024-05-04 20:17:32 +0100

    role_user and permission_role relationships

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -17,4 +17,9 @@ class Permission extends Model
     {
         return $this->belongsToMany(User::class);
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
