1609a22bb92eaa2ac50a799d4d2c587eb73d8022
Zsolt Domjan <domjanzsoo@yahoo.com>
2024-07-08 22:18:21 +0100

    profile image issues

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -104,7 +104,7 @@
                                 </button>
                             @else
                                 <span class="inline-flex rounded-md">
-                                    <x-profile-img :imgUrl="Auth::user()->profile_photo_path ?? asset('/storage/avatar/user.png')" size="8" />
+                                    <x-profile-img :imgUrl="Auth::user()->profile_photo_path ? asset(Auth::user()->profile_photo_path) : asset('/storage/avatar/user.png')" size="8" />
                                     <button type="button" class="inline-flex items-center px-2 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                         {{ Auth::user()->name }}
 
