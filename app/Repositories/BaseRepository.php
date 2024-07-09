43394e26fcdcb77a2b0e9d001a7bc74b7f672d5b
Zsolt Domjan <domjanzsoo@yahoo.com>
2024-07-09 13:18:35 +0100

    unused model import

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -7,7 +7,6 @@ use Illuminate\Pagination\LengthAwarePaginator;
 use Exception;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Permission;
 
 class BaseRepository implements BaseRepositoryInterface
 {
