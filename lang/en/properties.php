c6654c38e0cdd501c962563aca57533edf0abfbe
Zsolt Domjan <domjanzsoo@yahoo.com>
2024-07-10 13:15:33 +0100

    property translation

---
+++ lang/en/properties.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'property_entity' => 'property',
+    'properties' => 'Properties',
+    'properties_full' => 'All existing properties listed.',
+    'add' => 'Add Property',
+    'add_full' => 'Add a new property.',
+    'permission_name' => 'Property Name',
+    'room_number' => 'Number of Rooms',
+    'size' => 'Size',
+    'heating_type' => 'Heating Type',
+    'address' => 'Address',
+    'location' => 'Location',
+    'country' => 'Country',
+    'price' => 'Price',
+    'description' => 'Description'
+];
