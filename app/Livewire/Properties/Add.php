0a2b39f792c8f44b3580bc58d9dc0a902055b853
Zsolt Domjan <domjanzsoo@yahoo.com>
2024-07-11 20:36:05 +0100

    add property component creation

---
+++ app/Livewire/Properties/Add.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Livewire\Roles;
+
+use App\Contract\RoleRepositoryInterface;
+use App\Contract\PermissionRepositoryInterface;
+use Livewire\Component;
+use Illuminate\Auth\Access\AuthorizationException;
+
+class Add extends Component
+{
+    private $roleRepository;
+    private $permissionRepository;
+
+    protected $listeners = [
+        'role-permissions' => 'handlePermissions'
+    ];
+    
+    public array $state = [
+        'role_name'     => '',
+        'permissions'   => []
+    ];
+
+    protected $rules = [
+        'state.role_name' => 'required|unique:roles,name',
+        'state.permissions' => 'array'
+    ];
+
+    protected $messages = [
+        'state.role_name.required' => 'The role name is required.',
+        'state.role_name.unique' => 'A role with the given name already exists.'
+    ];
+
+    public function render()
+    {
+        return view('livewire.roles.add', [
+            'permissions' => $this->permissionRepository->getAll('name')
+        ]);
+    }
+
+    public function boot(
+        RoleRepositoryInterface $roleRepository,
+        PermissionRepositoryInterface $permissionRepository
+    )
+    {
+        $this->roleRepository = $roleRepository;
+        $this->permissionRepository = $permissionRepository;
+    }
+
+    public function handlePermissions(array $selections): void
+    {
+        $this->state['permissions'] = [];
+
+        foreach ($selections as $id => $selection) {
+            if ($selection['selected'] && !in_array($id, $this->state['permissions'])) {
+                array_push($this->state['permissions'], $id);
+            }
+        }
+    }
+
+    public function addRole(): void
+    {
+        if (!access_control()->canAccess(auth()->user(), 'add_role')) {
+            throw new AuthorizationException(trans('errors.unauthorized_action', ['action' => 'add role']));
+        }
+
+        $validatedData = $this->validate();
+
+        $this->roleRepository->createRole($validatedData['state']['role_name'], $validatedData['state']['permissions']);
+
+        $this->state['role_name'] = null;
+        $this->state['permissions'] = [];
+
+        $this->dispatch('toastr', ['type' => 'confirm', 'message' => 'Role created successfully!']);
+        $this->dispatch('role-permissions-submitted');
+        $this->dispatch('role-added');
+
+        return;
+    }
+}
--- resources/views/livewire/properties/all.blade.php
+++ resources/views/livewire/properties/all.blade.php
@@ -1,12 +1,20 @@
-<div>
-  <x-grid-paginated :data="$properties">
-    <div class="bg-white px-2 py-2">
-      <div class="flex">
-        <div :style="`background-image: url(${elmData.main_photo_path})`" class="inline-block w-16 h-16 bg-center bg-cover bg-no-repeat"></div>
-        <div class="ml-5 mt-3 text-sm" x-text="elmData.address + ', ' + elmData.location + ', ' + elmData.country"></div>
+<x-form-section>
+  <x-slot name="title">
+    {{ __('properties.properties') }}
+  </x-slot>
+  <x-slot name="description">
+    {{ __('properties.properties_full') }}
+  </x-slot>
+  <x-slot name="list">
+    <x-grid-paginated :data="$properties">
+      <div class="bg-white px-2 py-2">
+        <div class="flex">
+          <div :style="`background-image: url(${elmData.main_photo_path})`" class="inline-block w-16 h-16 bg-center bg-cover bg-no-repeat"></div>
+          <div class="ml-5 mt-3 text-sm" x-text="elmData.address + ', ' + elmData.location + ', ' + elmData.country"></div>
+        </div>
+        <div class="mt-3 px-3 pt-4 text-sm" x-text="elmData.description"></div>
       </div>
-      <div class="mt-3 px-3 pt-4 text-sm" x-text="elmData.description"></div>
-    </div>
-  </x-grid-paginated>
-</div>
+    </x-grid-paginated>
+  </x-slot>
+</x-form-section>
 
