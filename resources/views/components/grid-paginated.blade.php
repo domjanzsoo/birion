75fd7fb25a6136691d87b5c79e2f3565628bcba1
Zsolt Domjan <domjanzsoo@yahoo.com>
2024-08-09 23:37:49 +0100

    grid-paginated height fix

--- resources/views/components/grid-paginated.blade.php
+++ resources/views/components/grid-paginated.blade.php
@@ -1,12 +1,14 @@
 @props(['cols' => 3, 'items' => [], 'wrapperStyle' => ''])
 
-<div style="{{ $wrapperStyle }}">
-    <div class="grid grid-cols-{{ $cols }} gap-4">
-        @foreach($items as $elm)
-            <div class="mb-6" x-data="{ elmData: {{ json_encode($elm->toArray()) }} }" wire:key="elm-{{ $elm->id }}">
-                {{ $slot }}
-            </div>
-        @endforeach
+<div>
+    <div style="{{ $wrapperStyle }}">
+        <div class="grid grid-cols-{{ $cols }} gap-4">
+            @foreach($items as $elm)
+                <div class="mb-6" x-data="{ elmData: {{ json_encode($elm->toArray()) }} }" wire:key="elm-{{ $elm->id }}">
+                    {{ $slot }}
+                </div>
+            @endforeach
+        </div>
     </div>
     <div> {{ $items->links() }} </div>
 </div>
